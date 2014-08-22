4636eaf25e0710fc832810cc7beb7a2616adceb7
Master T <tom.deseyn@gmail.com>
2014-08-22 22:24:07 +0200

    Add dllexport en comment out mpegts

--- libgstreamersharpglue/gobject.c
+++ libgstreamersharpglue/gobject.c
@@ -21,13 +21,17 @@
 
 #include <glib-object.h>
 
-gint
+#ifndef MYAPI
+#define MYAPI __declspec(dllexport)
+#endif
+
+MYAPI gint
 gstsharp_g_closure_sizeof (void)
 {
   return sizeof (GClosure);
 }
 
-GType
+MYAPI GType
 gstsharp_g_type_from_instance (GTypeInstance * instance)
 {
   return G_TYPE_FROM_INSTANCE (instance);
